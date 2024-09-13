34360e3a8ac0597152518658111a9f23abb708a1
Keyur Bhatiya <115718562+keyurbhatiya@users.noreply.github.com>
2024-09-13 14:08:36 +0530

    asp.net

---
+++ Default.aspx
@@ -0,0 +1,18 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeFile="Default.aspx.cs" Inherits="_Default" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+    <div style="height: 48px">
+        <strong>SELECT ANY NAME :</strong> <asp:DropDownList ID="DropDownList1" runat="server" AutoPostBack="True" Height="20px" OnSelectedIndexChanged="DropDownList1_SelectedIndexChanged" Width="164px" BackColor="#FF9900" ForeColor="Black" style="font-weight: 700"></asp:DropDownList>
+    
+    </div>
+       <center> <asp:Image ID="Image1" runat="server" Height="566px" Width="1282px"/></center>
+    </form>
+</body>
+</html>
---
+++ Default.aspx.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+public partial class _Default : System.Web.UI.Page
+{
+    protected void Page_Load(object sender, EventArgs e)
+    {
+        if(!Page.IsPostBack)
+        {
+            String[] str = new String[] { " Select ", "AVENGER'S LOGO", "IRON_MAN","CAPTAIN_MARVEL","THOR","CAPTAIN_AMERICA","SPIDER_MAN","DOCTOR_STRANGE","NATASHA","WANDA","HULK" };
+            for (int i=0; i<str.Length; i++)
+            {
+                DropDownList1.Items.Add(str[i]);
+            }
+        }
+    }
+    protected void DropDownList1_SelectedIndexChanged(object sender, EventArgs e)
+    {
+        String str = DropDownList1.Text;
+        Image1.ImageUrl = "/IMG/" + str + ".JPG";
+    }
+}
